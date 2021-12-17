defaf27642125f2230f68a0556e8e24f534caaec
isomoth <ic.gonzalez35@hotmail.com>
2021-12-17 21:58:24 +0100

    added Swagger documentation + completed README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Top 100 Music Tracks - A MongoDB REST API
 
-This is my second API project. I learned to "translate" Express queries from a previous project to Mongoose. The dataset consists of the top 100 streamed songs of all time on Spotify. The source of the data was Kaggle.com, which in turn retrieved it from http://organizeyourmusic.playlistmachinery.com/.
+This is my second API project, documented via Swagger. I learned to "translate" Express queries from a previous project to Mongoose. The dataset consists of the top 100 streamed songs of all time on Spotify. The source of the data was Kaggle.com, which in turn retrieved it from http://organizeyourmusic.playlistmachinery.com/.
 
 ## Features
 
@@ -8,20 +8,23 @@ This is my second API project. I learned to "translate" Express queries from a p
 - Endpoints that return a collection of results: Get all tracks, filter pop tracks, filter tracks by workout or calm moods.
 - Endpoints that return a single result: Get track by ID and query parameters: trackName, artistName, genre, year, bpm.
 - Error handling by returning 404 status codes when the item is not found.
+- Swagger documentation.
 
 ## Production process
 
 - This is an enhancement of my Express API project: https://github.com/isomoth/project-express-api
 - The first route was /tracks. First I console.logged the data and then implemented it as the API response.
 - Then I proceeded to do the routes that return the collections, and finally the single results (including the ones with query params).
-  I created filters that simulate a rudimentary playlist mechanism. One of the routes displays tracks that are suitable for a party by filtering by the danceability parameter. The other two routes filter tracks through the energy parameter to present a collection of workout songs and another with a calm mood.
-- Last but not least, I added error handling to all routes (this is an improvement from the Express API project, where not all of them had it).
-- Another enhancement from last week was the implementation of pagination (with the limit method).
+- I created filters that simulate a rudimentary playlist mechanism. One of the routes displays tracks that are suitable for a party by filtering by the danceability parameter. The other two routes filter tracks through the energy parameter to present a collection of workout songs and another with a calm mood.
+- The next step was adding some 404 error handling to all routes (this is an improvement from the Express API project, where not all of them had it). In future implementations, I might add error handling for other use cases.
+- Last but not least, I documented the API through Swagger.
 
 ## Challenges and lessons learned
 
 - The most difficult part about this project was the deployment. Connecting Heroku with MongoDB Cloud Atlas took a while to figure out. But the app's documentation combined with a detailed inspection of Heroku logs helped a lot.
+- The second biggest challenge was the API documentation through Swagger. I had to search for different sources because the official docs were a bit confusing. And it also took a bit of practice to get used to YAML.
 
 ## View it live
 
-https://top-tracks-isabel-mongodb.herokuapp.com/
+- Documented version: https://top-tracks-isabel-mongodb.herokuapp.com//docs
+- Heroku version: https://top-tracks-isabel-mongodb.herokuapp.com/
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import mongoose from 'mongoose';
 import listEndpoints from 'express-list-endpoints';
 import topMusicData from './data/top-music.json';
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
+
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo';
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = Promise;
@@ -14,6 +17,26 @@ mongoose.Promise = Promise;
 //   PORT=9000 npm start
 const port = process.env.PORT || 8080;
 const app = express();
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Spotify Songs API',
+      version: '1.0.0',
+      description:
+        'A mongo-backed API. The dataset consists of the top 100 streamed songs of all time on Spotify.',
+      contact: {
+        name: 'Isabel González',
+        email: 'ic.gonzalez35@hotmail.com',
+        url: 'https://github.com/isomoth/',
+      },
+    },
+  },
+  apis: ['./server.js'], // files containing annotations as above
+};
+const swaggerDocs = swaggerJsdoc(options);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
@@ -34,6 +57,18 @@ app.get('/', (req, res) => {
 });
 
 // Send list of all endpoints
+
+/**
+ * @swagger
+ * /endpoints:
+ *   get:
+ *     summary: List all API endpoints
+ *     tags:
+ *      - Endpoints
+ *     responses:
+ *       200:
+ *         description: Successful response.
+ */
 app.get('/endpoints', (req, res) => {
   res.send(listEndpoints(app));
 });
@@ -74,8 +109,36 @@ if (process.env.RESET_DB) {
 // Examples: /tracks/?year=2016
 // Examples: /tracks/?bpm=80
 
+/**
+ * @swagger
+ /tracks:
+ *   get:
+ *     summary: List all tracks, optional filters by query parameters
+ *     parameters:
+ *      - name: trackName
+ *        in: query
+ *        required: false
+ *        format: string
+ *      - name: artistName
+ *        in: query
+ *        required: false
+ *        format: string
+ *      - name: genre
+ *        in: query
+ *        required: false
+ *        format: string
+ *      - name: year
+ *        in: query
+ *        required: false
+ *        format: string
+ *     tags: 
+ *      - Tracks
+ *     responses:
+ *       200:
+ *         description: Successful response.
+ */
 app.get('/tracks/', async (req, res) => {
-  let tracks = await Track.find(req.query).limit(10);
+  let tracks = await Track.find(req.query);
   if (tracks) {
     // Get tracks for your party (i.e. danceability based on the number you specify).
     // Test with different values. For example, choose 70 if you're looking for more energetic dances.
@@ -94,6 +157,24 @@ app.get('/tracks/', async (req, res) => {
 });
 
 // Get track by ID
+
+/**
+ * @swagger
+ * /tracks/id/{id}:
+ *   get:
+ *     summary: Get tracks by id
+ *     parameters:
+ *      - name: id
+ *        in: path
+ *        required: true
+ *        description: Get an id from the "tracks" query above. Click on "Try it out" and then fill in the song id.
+ *        format: integer
+ *     tags:
+ *      - Single result
+ *     responses:
+ *       200:
+ *         description: Successful response.
+ */
 app.get('/tracks/id/:id', async (req, res) => {
   try {
     const trackById = await Track.findById(req.params.id);
@@ -108,6 +189,18 @@ app.get('/tracks/id/:id', async (req, res) => {
 });
 
 // Get all songs by Billie Eilish
+
+/**
+ * @swagger
+ * /tracks/artist/billie_eilish:
+ *   get:
+ *     summary: Get all tracks by Billie Eilish
+ *     tags:
+ *      - Artist Collection
+ *     responses:
+ *       200:
+ *         description: Successful response.
+ */
 app.get('/tracks/artist/billie_eilish', (req, res) => {
   Track.find({ artistName: 'Billie Eilish' }, (error, data) => {
     if (error) {
@@ -119,6 +212,18 @@ app.get('/tracks/artist/billie_eilish', (req, res) => {
 });
 
 // Get tracks from the pop genre
+
+/**
+ * @swagger
+ * /tracks/genre/pop:
+ *   get:
+ *     summary: Get all tracks within the pop genre
+ *     tags:
+ *      - Genre Collection
+ *     responses:
+ *       200:
+ *         description: Successful response.
+ */
 app.get('/tracks/genre/pop', (req, res) => {
   Track.find({ genre: 'pop' }, (error, data) => {
     if (error) {
@@ -130,6 +235,18 @@ app.get('/tracks/genre/pop', (req, res) => {
 });
 
 // Get tracks for your workout, i.e. energy > 80. No parameters needed.
+
+/**
+ * @swagger
+ * /tracks/playlists/workout/:
+ *   get:
+ *     summary: Get all tracks with energy > 80
+ *     tags:
+ *      - Playlists
+ *     responses:
+ *       200:
+ *         description: Successful response.
+ */
 app.get('/tracks/playlists/workout/', (req, res) => {
   Track.find({ energy: { $gte: 80 } }, (error, tracks) => {
     if (error) {
@@ -141,6 +258,18 @@ app.get('/tracks/playlists/workout/', (req, res) => {
 });
 
 // Get calm tracks for a cozy time at home, i.e. energy < 20. No parameters needed.
+
+/**
+ * @swagger
+ * /tracks/playlists/calm/:
+ *   get:
+ *     summary: Get all tracks with energy < 20
+ *     tags:
+ *      - Playlists
+ *     responses:
+ *       200:
+ *         description: Successful response.
+ */
 app.get('/tracks/playlists/calm/', (req, res) => {
   Track.find({ energy: { $lte: 20 } }, (error, tracks) => {
     if (error) {
